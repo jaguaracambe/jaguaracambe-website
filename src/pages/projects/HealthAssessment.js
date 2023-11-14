b7bdaa48a152d2498c7bffce05597b150e757e5c
dataskeptic <mateus_henrique2011@outlook.com>
2023-11-14 16:44:56 -0300

    creating definition section of helth assesment

--- src/pages/projects/HealthAssessment.js
+++ src/pages/projects/HealthAssessment.js
@@ -6,6 +6,8 @@ import React, { useState, useEffect } from "react";
 
 import health_intro  from '../../../public/health_assessment/health_intro.jpg'
 import health_intro_mobile  from '../../../public/health_assessment/health_intro_mobile.jpg'
+import researcher_working  from '../../../public/health_assessment/researcher_working.jpg'
+import researcher_working_mobile  from '../../../public/health_assessment/researcher_working_mobile.jpg'
 
 
 export default function HealthAssessment() {
@@ -41,6 +43,27 @@ export default function HealthAssessment() {
 
         <div className={styles.home_content}>
           <h1>AVALIAÇÃO SANITÁRIA</h1>
+        </div>
+      </section>
+
+      <section className={styles.health_definition_section}>
+        <Image
+          src={windowWidth > 650 ? researcher_working : researcher_working_mobile}
+          atl="Pesquisadores da ONG Jaguaracambé fazendo avaliaçao sanitária de um Lobó Guará"
+          style={{
+            width: windowWidth > 650 ? "auto" : "100vw",
+            height: windowWidth > 650 ? "100vh" : "auto",
+          }}
+        />
+
+        <div className={styles.health_definition_content}>
+          <h1>O PROJETO</h1>
+          <p>
+          A proximidade das áreas preservadas com áreas urbanas e rurais vêm possibilitando um contato maior entre as espécies de animais silvestres com os animais domésticos e humanos, esse fator aliado à crise da biodiversidade e à diminuição das espécies, favorece a adaptação de microrganismos causadores de doenças, o que consequentemente pode facilitar o surgimento de novas enfermidades. Além disso, o estabelecimento de doenças infecciosas pode contribuir para a extinção local de várias espécies de animais selvagens, o que por sua vez, também ocasiona a redução das populações de espécies.
+          </p>
+          <p>
+          Todos esses fatores estão diretamente relacionados ao conceito de Saúde Única, que reconhece a importância do equilíbrio entre a saúde humana, animal e ambiental para que sejam estabelecidas políticas públicas adequadas de prevenção e controle de enfermidades.
+          </p>
         </div>
       </section>
 
