32d170945dc9857c1d325777ba4c60133f448314
dataskeptic <mateus_henrique2011@outlook.com>
2023-11-27 01:20:37 -0300

    creating the mobile nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
-import React from 'react';
 import styles from '../styles/Header.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import React, { useState } from 'react';
+import logo from '/public/logo.png'
+
 
 
 const Header = () => {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image src="/logo.png" alt="YLogo da ONG jaguaracambé" width={77} height={77} />
+    <header className={`${styles.header} ${isActive ? styles.active : ''}`}>
+      <div className={styles.top_header}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="Logo da ONG jaguaracambé" width={49} height={49}/>
+        </div>
+        <button className={styles.btn_mobile} onClick={toggleMenu}>
+            <span className={styles.hamburger}></span>
+        </button>
       </div>
       <nav className={styles.nav}>
-        <Link href="/#about_section" className={styles.link}>SOBRE A ONG</Link>
-        <Link href="/#projects" className={styles.link}>PROJETOS</Link>
-        <Link href="/#species" className={styles.link}>ESPÉCIES</Link>
-        <Link href="/#support" className={styles.link}>APOIE</Link>
+        <Link href="/#about_section" className={styles.link} onClick={toggleMenu}>SOBRE A ONG</Link>
+        <Link href="/#projects" className={styles.link} onClick={toggleMenu}>PROJETOS</Link>
+        <Link href="/#species" className={styles.link} onClick={toggleMenu}>ESPÉCIES</Link>
+        <Link href="/#support" className={styles.link} onClick={toggleMenu}>APOIE</Link>
       </nav>
     </header>
   );
